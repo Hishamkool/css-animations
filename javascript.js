ec2430fa3bcaf3931b458a4851d68b6a80725a8b
Hisham <linkedin.hisham@gmail.com>
2025-08-11 14:26:44 +0530

    css animations with mario pngs

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// const circle = document.querySelector(".circle");
+// const cloud = document.querySelector(".wrapper img");
+
+// circle.addEventListener('mouseenter', () => {
+//     cloud.style.transform = 'translateX(-50%)';
+//     cloud.style.transition = 'transform 1s ease'
+// });
+
+// circle.addEventListener('mouseleave', () => {
+//     cloud.style.transform = "translateX(120px)";
+// });
